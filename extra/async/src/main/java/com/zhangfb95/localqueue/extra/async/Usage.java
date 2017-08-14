a51315b6b858e5b15cd7e0fe43818e3310250df5
zhangfb <zhangfb@shishike.com>
2017-08-14 16:42:51 +0800

    add multi project support

---
+++ extra/async/src/main/java/com/zhangfb95/localqueue/extra/async/Usage.java
@@ -0,0 +1,14 @@
+package com.zhangfb95.localqueue.extra.async;
+
+import com.zhangfb95.localqueue.logic.core.DefaultLocalQueue;
+import com.zhangfb95.localqueue.logic.core.LocalQueue;
+
+/**
+ * @author zhangfb
+ */
+public class Usage {
+
+    public void calc() {
+        LocalQueue localQueue = new DefaultLocalQueue(null);
+    }
+}
