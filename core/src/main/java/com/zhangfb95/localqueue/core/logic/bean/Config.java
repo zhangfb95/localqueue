71e04c5eb5f4a6d4b8656c65b69caf131c770311
zhangfb <zhangfb@shishike.com>
2018-01-08 17:41:03 +0800

    去掉尾注释

--- core/src/main/java/com/zhangfb95/localqueue/core/logic/bean/Config.java
+++ core/src/main/java/com/zhangfb95/localqueue/core/logic/bean/Config.java
@@ -10,12 +10,35 @@ import java.io.File;
 @Data
 public class Config {
 
-    private String storageDir; // 存储位置
-    private Integer dataFileCapacity; // 数据文件容量
-    private String idxFileName = "localqueue_idx.db"; // 索引文件名称
-    private String dataFileName = "localqueue_data_%d.db";  // 数据文件名称，其中"%d"为文件自增序号
-    private Long gcExceedTime; // 回收超时时间
-    private String gcTimeUnit; // 回收超时时间粒度，
+    /**
+     * 存储位置
+     */
+    private String storageDir;
+
+    /**
+     * 数据文件容量
+     */
+    private Integer dataFileCapacity;
+
+    /**
+     * 索引文件名称
+     */
+    private String idxFileName = "localqueue_idx.db";
+
+    /**
+     * 数据文件名称，其中"%d"为文件自增序号
+     */
+    private String dataFileName = "localqueue_data_%d.db";
+
+    /**
+     * 回收超时时间
+     */
+    private Long gcExceedTime;
+
+    /**
+     * 回收超时时间粒度
+     */
+    private String gcTimeUnit;
 
     /**
      * 获取索引文件路径
